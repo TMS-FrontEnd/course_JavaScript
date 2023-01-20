365fba5c7eb6580e520f2f1100d53889544ef99a
IgorLenko <mk2008@mail.ru>
2023-01-20 21:26:26 +0300

    feat: todo

--- lesson-11/script.js
+++ lesson-11/script.js
@@ -1,11 +1,29 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage
 
-// window.addEventListener('storage', (event) => {
-//   console.log(event);
-// })
 
+/*
+  window.addEventListener('storage', (event) => {
+    console.log(event);
+  })
+*/
 
+/*
+  const data = localStorage.getItem('key') // получаем данные по ключу 
+  localStorage.setItem('key-string', 'name-string') // создает/меняет storage по ключ-значение
+  localStorage.removeItem('key-string')  // удаляет по ключу
+  localStorage.clear() // очищает весь storage
+  localStorage.key('key-string') // проверяет наличие данных по ключу
+  localStorage.length // получаем длинну всех storage
+*/
 
+// ключ и значение ТОЛЬКО строки по типу
+// для преобразования объекта в строку и спользуем глобальный объект JSON
+ /*
+  const user = {
+    name: 'Ivan',
+    age: 45,
+  };
 
-
-
+  преобразуем в строку - JSON.stringify(user)
+  преобразуем обратно в объект - JSON.parse(localStorage.getItem('user'))
+*/
